0e034d940218f391b4fde3467aa4a6ea8f1e9586
Fede Ramirez <i@2fd.me>
2017-11-08 01:49:20 -0300

    add metadata to GrpcError

--- lib/errors/grpc/GrpcError.ts
+++ lib/errors/grpc/GrpcError.ts
@@ -3,4 +3,8 @@ import {Grpc, Http} from "../../codes";
 export class GrpcError extends Error {
     public status: Grpc;
     public statusCode: Http;
+    constructor(message: string, metadata: object | null = null) {
+        super(message);
+        Object.assign(this, metadata);
+    }
 }
